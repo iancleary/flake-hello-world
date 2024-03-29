a5da2a4ed79d240f1347c8202815bf9646a14e87
Ian Cleary <github@iancleary.me>
2024-03-28 17:03:28 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
-    # systems.url = "github:nix-systems/default-linux";
-
-    # hyprland.url = "github:hyprwm/Hyprland";
-
-    # hyprland-contrib = {
-    #   url = "github:hyprwm/contrib";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
   outputs = inputs @ {
